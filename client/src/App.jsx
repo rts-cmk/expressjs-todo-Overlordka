214592051f0d7e2b8b1e15e62897bf1977d952c4
Overlordka <kopary787@gmail.com>
2025-11-27 10:35:02 +0100

    feat: todo post delete functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,81 @@
+import { useEffect } from "react"
 import { useState } from "react"
 
 function App() {
 
   const [status, setStatus] = useState(false)
   const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
 
-  async function sendRequest() {
-    const response = await fetch("/todos", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ status, todo })
-    })
+
+  async function loadData() {
+    const response = await fetch("/todos")
 
     const data = await response.json()
+    setTodos(data)
     console.log(data)
   }
 
+  useEffect(() => {
+    loadData()
+  }, [])
+
+  async function sendRequest(event) {
+    event.preventDefault()
+    if (todo == "") {
+
+    } else {
+      const response = await fetch("/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ status, todo })
+      })
+
+      await response.json()
+    }
+
+
+    loadData()
+
+    setStatus(false)
+    setTodo("")
+  }
+
+  async function deleteTodo(id) {
+    await fetch(`/todos/${id}`, {
+      method: "DELETE"
+    })
+
+    loadData()
+  }
+
+
   return (
     <>
-      <input
-        type="text"
-        value={todo}
-        onChange={e => setTodo(e.target.value)}
-        placeholder="To Do"
-      />
-      <input
-        type="checkbox"
-        checked={status}
-        onChange={e => setStatus(e.target.checked)}
-      />
+      <form>
+        <input
+          type="text"
+          value={todo}
+          onChange={e => setTodo(e.target.value)}
+          placeholder="To Do"
+        />
+        <input
+          type="checkbox"
+          checked={status}
+          onChange={e => setStatus(e.target.checked)}
+        />
 
-      <button onClick={sendRequest}>Send</button>
+        <button onClick={sendRequest}>Send</button>
+      </form>
+      <ul>
+        {todos.map(t => (
+          <li key={t.id}>
+            {t.todo} - {t.status ? "done" : "not done"} <button onClick={() => deleteTodo(t.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -6,13 +6,30 @@ const PORT = 3000
 app.use(cors())
 app.use(express.json())
 
+let todos = []
+let nextId = 1
+
 
 app.post ("/todos", (req, res) => {
     const { status, todo } = req.body 
 
-    console.log("Status:", status, "Todo:", todo)
+    const newTodo = { id: nextId++, "status": status, "todo": todo }
+    todos.push (newTodo)
 
-    res.json({ status, todo })
+    console.log(newTodo)
+
+    res.json(newTodo)
+})
+
+app.get("/todos", (req, res) => {
+  res.json(todos)
+})
+
+app.delete("/todos/:id", (req, res) => {
+    const id = Number(req.params.id)
+
+    todos = todos.filter(t => t.id !== id)
+    res.json({ message: "Deleted", id})
 })
 
 
